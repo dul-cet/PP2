f9847b659f3fa746db9ca7637363359812cedc89
Moldir_Alimgazy <a_moldir@kbtu.kz>
2024-01-20 23:49:35 +0600

    commit

---
+++ Lab_2/6-dictionaries/6-loop_dictionaries.py
@@ -0,0 +1,14 @@
+for x in thisdict:
+  print(x)
+
+for x in thisdict:
+  print(thisdict[x])
+
+for x in thisdict.values():
+  print(x)
+
+for x in thisdict.keys():
+  print(x)
+
+for x, y in thisdict.items():
+  print(x, y)
