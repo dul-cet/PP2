81626e18a631043da19b3f508e25776c5413f138
Moldir_Alimgazy <a_moldir@kbtu.kz>
2024-02-02 23:14:10 +0600

    commit

--- Lab_3/functions_1/1.py
+++ Lab_3/functions_1/1.py
@@ -1,10 +1,12 @@
 """
 1. A recipe you are reading states how many grams you need for the ingredient.
-Unfortunately, your store only sells items in ounces. Create a function to convert grams to ounces. 
+Unfortunately, your store only sells items in ounces.
+Create a function to convert grams to ounces. 
 `ounces = 28.3495231 * grams` """
+
+grams = int(input())
 def convert(grams):
     ounces = 28.3495231 * grams
     return ounces
 
-grams = int(input())
 print(convert(grams))
---
+++ Lab_3/functions_1/2.py
@@ -0,0 +1,12 @@
+"""
+2. Read in a Fahrenheit temperature. 
+Calculate and display the equivalent centigrade temperature.
+The following formula is used for the conversion:
+`C = (5 / 9) * (F – 32)`"""
+
+fahrenheit = float(input())
+def temperature(fahrenheit):
+    celsius = (5 / 9) * (fahrenheit - 32)
+    return celsius
+    
+print(temperature(fahrenheit))
