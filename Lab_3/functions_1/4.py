3bd91303f5cca0f4384362292c350b10b6892f6e
Moldir_Alimgazy <a_moldir@kbtu.kz>
2024-02-05 10:39:46 +0600

    commit

--- Lab_3/functions_1/4.py
+++ Lab_3/functions_1/4.py
@@ -4,14 +4,16 @@ Write a function `filter_prime` which will take list of numbers as an agrument
 and returns only prime numbers from the list. """
 
 def is_prime(n):
-    if n < 2:
+    if n == 0:
         return False
+    if n < 0:
+        n = -n
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
     return True
 
-lst = [int(item) for item in input().split()]
+lst = list(map(int, input().split()))
 print(lst)
 
 prime_numbers = [item for item in lst if is_prime(item)]
