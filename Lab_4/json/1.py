c151e26b9d31659a2363f73e53896ba0d7f7b86c
Moldir_Alimgazy <a_moldir@kbtu.kz>
2024-02-24 20:17:57 +0600

    commit

--- Lab_4/json/1.py
+++ Lab_4/json/1.py
@@ -2,3 +2,12 @@
 # create output that resembles the following by parsing the included JSON file.
 
 import json
+print("Interface Status")
+print("================================================================================")
+print("DN                                                 Description           Speed    MTU  ")
+print("-------------------------------------------------- --------------------  ------  ------")
+
+with open("sample-data.json", "r") as js_file:
+    j = json.load(js_file)
+for x in j["imdata"]:
+    print(f"{x["l1PhysIf"]["attributes"]["dn"]}  {x["l1PhysIf"]["attributes"]["speed"]}   {x["l1PhysIf"]["attributes"]["mtu"]}")
